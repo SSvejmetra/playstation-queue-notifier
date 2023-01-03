0ca6e1b47b3e492589e5bd041c57af93ae32ce04
Vladislavs Poznaks <vladislavs.poznaks@gmail.com>
2023-01-03 21:52:28 +0200

    Added chat model, factory, migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,4 +47,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Order::class);
     }
+
+    public function chats(): HasMany
+    {
+        return $this->hasMany(Chat::class);
+    }
 }
